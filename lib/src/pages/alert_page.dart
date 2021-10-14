216ea746bf429bf3e82061fbe2c5903302f3e347
Alex Zafra <azafrom731@g.educaand.es>
2021-10-14 14:36:58 +0200

    Añadida una alerta

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,7 +9,14 @@ class AlertPage extends StatelessWidget {
         title: Text("Alert Page"),
       ),
       body: Center(
-        child: Text("Texto", style: TextStyle(fontSize: 30.0),),
+        child: ElevatedButton(
+          onPressed: () => _mostrarAlert(context), 
+          child: Text("Mostrar Alerta"),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            shape: StadiumBorder()
+          )
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
@@ -17,6 +24,41 @@ class AlertPage extends StatelessWidget {
         },
         child: Icon(Icons.arrow_left_rounded),
       ),
+    );
+  }
+
+  _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context, 
+      barrierDismissible: true,
+      builder: (context){
+        
+        return AlertDialog(
+          title: Text("Alerta"),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Este es el contenido de la alerta"),
+              SizedBox(height: 20),
+              FlutterLogo(size: 80,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text("Cancelar"),
+              onPressed: () => Navigator.of(context).pop(), 
+            ),
+            TextButton(
+              child: Text("Ok"),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },   
+            ),
+          ],
+        );
+      },
+      
     );
   }
 }
